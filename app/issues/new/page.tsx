f8ff6358858e34ab3e2e2aeb6733b6ba03e5134e
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-10-12 04:08:41 +0530

    handle form submission

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,12 +1,34 @@
 "use client";
 import { Input } from "@/components/ui/input";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const route = useRouter();
+  const { register, control, handleSubmit } = useForm<IssueForm>();
+  console.log(register("title"));
   return (
-    <form className="pt-10 px-9 space-y-4">
+    <form
+      className="pt-10 px-9 space-y-4"
+      onSubmit={handleSubmit(async (data) => {
+        try {
+          await axios.post("/api/issues", data);
+          route.push("/issues");
+        } catch (error) {
+          console.error("Error submitting issue:", error);
+        }
+      })}
+    >
       <div>
         <Label htmlFor="issue-title">Issue Title</Label>
         <Input
@@ -14,15 +36,23 @@ const NewIssuePage = () => {
           type="text"
           placeholder="Enter issue title"
           className="p-5 text-lime-300"
+          {...register("title")}
         />
       </div>
       <div>
         <Label htmlFor="issue-description">Issue Description</Label>
-        <Textarea
-          id="issue-description"
-          placeholder="Write your issue description"
-          className="p-4 min-h-[150px]"
-        />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE
+              id="issue-description"
+              placeholder="Write your issue description"
+              className="p-4 min-h-[150px]"
+              {...field}
+            />
+          )}
+        ></Controller>
       </div>
       <Button type="submit">Submit Issue</Button>
     </form>
